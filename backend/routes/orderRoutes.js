07309164bda686e24ac306bfceee667d2d61c52a
Bao Toan <s4045102@rmit.edu.vn>
2025-09-03 00:15:57 +0700

    order display(working) ToDo display resjson

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,14 +1,32 @@
-// const express = require("express");
-// const router = express.Router();
-// const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 
-// const User = require("../models/User");
-// const Customer = require("../models/Customer");
-// const Vendor = require("../models/Vendor");
-// const Shipper = require("../models/Shipper");
-// const Product = require('../models/Product')
-// const DistributionHub = require("../models/DistributionHub");
-// const ShoppingCart = require("../models/ShoppingCart");
-// const Order = require("../models/Order");
+const User = require("../models/User");
+const Customer = require("../models/Customer");
+const Vendor = require("../models/Vendor");
+const Shipper = require("../models/Shipper");
+const Product = require('../models/Product')
+const DistributionHub = require("../models/DistributionHub");
+const ShoppingCart = require("../models/ShoppingCart");
+const Order = require("../models/Order");
 
-// router.get()
+router.get("/:userId", async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const customer = await Customer.findOne({ user: userId })
+        if (!customer) {
+            res.status(500).json({ error: 'Failed to fetch orders(Couldnt find user).' });
+        }
+        const orders = await Order.find({ customer: customer._id })
+            .populate("customer")
+            .populate("distributionHub")
+            .populate("items.product");
+        res.json(orders);
+    } catch (err) {
+        console.error('❌ Failed to load orders:', err);
+        res.status(500).json({ error: 'Failed to fetch orders.' });
+    }
+});
+
+module.exports = router;
--- frontend/pages/Customer/OrderView.jsx
+++ frontend/pages/Customer/OrderView.jsx
@@ -5,8 +5,35 @@ import axios from "axios";
 
 export default function OrderView() {
     const user = useSelector(authSelect.user);
+    const [orders, setOrders] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
-    
+    const fetchOrders = async () => {
+        if (!user?._id) return;
+        setLoading(true);
+        setError("");
+        try {
+            const res = await axios.get(
+                `http://localhost:5000/api/orders/${user._id}`,
+                { withCredentials: true }
+            );
+            console.log(res.data);
+            setOrders(res.data);
+        } catch (err) {
+            console.error("❌ Failed to fetch orders:", err);
+            setError("Failed to load orders. Please try again.");
+        } finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        fetchOrders();
+      }, [user]);
+
+    return (
+        <>
+
+        </>
+    )
 }
