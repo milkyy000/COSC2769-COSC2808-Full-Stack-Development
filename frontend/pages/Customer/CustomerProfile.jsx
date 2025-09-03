e1eb12f0171cfe8c204f3673aae77047aa6a387d
Bao Toan <s4045102@rmit.edu.vn>
2025-09-03 14:57:57 +0700

    cart route hotfix, add author tag for files

--- frontend/pages/Customer/CustomerProfile.jsx
+++ frontend/pages/Customer/CustomerProfile.jsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { useRouteError } from "react-router-dom";
 import { Link } from "react-router-dom";
--- frontend/pages/Customer/ShoppingCartView.jsx
+++ frontend/pages/Customer/ShoppingCartView.jsx
@@ -1,3 +1,10 @@
+// RMIT University Vietnam
+// Course: COSC2769 - Full Stack Development
+// Semester: 2025B
+// Assessment: Assignment 02
+// Author: Nguyen Bao Toan
+// ID: s4045102
+
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { authSelect } from "../../src/redux/authSlice";
